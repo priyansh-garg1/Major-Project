21e8fe628dd7866945a4a12ef540688457e61a42
Priyansh Garg <143625974+priyansh-garg1@users.noreply.github.com>
2023-12-23 16:54:13 +0530

    Update listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -2,7 +2,7 @@ const Listing = require("../models/listing");
 
 module.exports.index = async (req,res) => {
     const allListing = await Listing.find({});
-    res.render("listings/index.ejs");
+    res.render("listings/index.ejs",{allLisitng});
 };
 
 module.exports.renderNewForm = (req,res) => {
