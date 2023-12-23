e8b5469a2c61c6950854fe8ea93898d91fcc0f2b
Priyansh Garg <143625974+priyansh-garg1@users.noreply.github.com>
2023-12-23 17:33:52 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -19,7 +19,11 @@ const listingSchema = new Schema({
       type: Schema.Types.ObjectId,
       ref: "Review"
     }
-  ]
+  ],
+  owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+  },
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
